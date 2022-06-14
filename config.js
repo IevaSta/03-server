f1fe6498f4caa0951ee1f6dc9e24f128696ecc13
IevaSta <ieva@kontraforma.lt>
2022-06-14 15:40:28 +0300

    Cache period from config

--- config.js
+++ config.js
@@ -10,6 +10,10 @@ config.dev = {
         password: 'admin',
         database: 'batai',
     },
+    cache: {
+        default: 0,
+        period: {},
+    }
 }
 
 config.prod = {
@@ -22,6 +26,25 @@ config.prod = {
         password: 'xxxxxxxx',
         database: 'batai_zzz',
     },
+    cache: {
+        default: 60 * 60,
+        period: {
+            css: 60 * 60,
+            js: 60 * 60,
+            svg: 60 * 60,
+            png: 60 * 60,
+            jpg: 60 * 60,
+            ico: 60 * 60,
+            woff2: 60 * 60,
+            woff: 60 * 60,
+            ttf: 60 * 60,
+            otf: 60 * 60,
+            eot: 60 * 60,
+            webmanifest: 60 * 60,
+            pdf: 60 * 60,
+            json: 60 * 60,
+        },
+    }
 }
 
 //process.env.NODE_ENV priskiriam kintamajam ---- issitraukiam, ka zmogus parase, jeigu parase
