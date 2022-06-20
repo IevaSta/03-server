a17e61fdce397c1b1266661f3ff0d16a5b9eb3df
IevaSta <ieva@kontraforma.lt>
2022-06-20 16:53:28 +0300

    User Logged in state

--- pages/home.js
+++ pages/home.js
@@ -1,14 +1,14 @@
 import { PageTemplate } from "../lib/PageTemplate.js";
 
 class PageHome extends PageTemplate {
-    constructor() {
-        super();
+    constructor(data) {
+        super(data);
         this.title = 'Home | Server';
     }
 
     mainHTML() {
-        return `<div class="row">
-        <h1>Home page 🎅</h1>
+        return `<div class="row main-main">
+            <img src="https://i.pinimg.com/564x/52/ac/6f/52ac6f4b3c233b2827959254f1c7f4df.jpg" alt="bbs">
         </div>`;
     }
 }
