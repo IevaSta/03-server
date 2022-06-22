793dfe8b755d47aabd32a37fe9216ce3ce8d22f7
IevaSta <ieva@kontraforma.lt>
2022-06-22 15:59:06 +0300

    Register form elements

--- public/js/pages/register.js
+++ public/js/pages/register.js
@@ -1,1 +1,11 @@
-console.log('register.js turinys');
+const formDOM = document.querySelector('.form');
+const inputsDOM = formDOM.querySelectorAll('input');
+const submitDOM = formDOM.querySelector('button');
+
+if (submitDOM) {
+    submitDOM.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log('Siunciama informacija')
+        console.log(inputsDOM);
+    })
+}
